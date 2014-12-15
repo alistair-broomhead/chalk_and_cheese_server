759d37335145b9e9ecddf0c1363ed39611ebb79f
Alistair Broomhead <alistair.broomhead@gmail.com>
2014-12-15 12:04:51 +0000

    allow cross-origin - this will need to be locked down later

--- chalk_and_cheese_server/server.py
+++ chalk_and_cheese_server/server.py
@@ -174,6 +174,11 @@ def add_endpoints(route):
         return table.stand(user=user)
 
 
+@bottle.hook('after_request')
+def enable_cors():
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+
+
 def run(app=bottle):
     add_endpoints(app.route)
     app.run()
